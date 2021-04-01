4b61f5698b4536f448d49d61469952478f394d50
anandtvdeveloper <81522407+anandtvdeveloper@users.noreply.github.com>
2021-04-01 07:38:11 +0530

    Delete EasyCart directory

--- EasyCart/cartdataapp/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class CartdataappConfig(AppConfig):
-    name = 'cartdataapp'
--- EasyCart/homescreen/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from homescreen import views
-app_name='homescreen'
-
-urlpatterns = [
-    path('',views.home,name='home'),
-]
